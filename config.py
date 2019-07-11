9b148a5a3211d6d0277b364bd4dbc7d702186f62
Roy Chela <roychela@gmail.com>
2019-07-11 15:35:06 +0300

    Completed

---
+++ config.py
@@ -0,0 +1,51 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://roy:1234567@localhost/grao'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Pitches'
+    SENDER_EMAIL = 'roychela@gmail.com'
+class ProdConfig(Config):
+    '''
+    Pruduction configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://roy:1234567@localhost/grao3'
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://roy:1234567@localhost/grao3'
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
